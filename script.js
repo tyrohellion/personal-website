ed653976a5a24428b17f5cc289ace9bb2d2ab8a8
tyrohellion <evanmarchesani@gmail.com>
2024-03-20 03:36:12 -0400

    main layout

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
+document.body.classList.toggle('dark-theme');
+
 document.getElementById('dark-theme').addEventListener('click', function() {
     document.body.classList.toggle('dark-theme');
 });
 
-
-function scrollToElement() {
+function scrollToPortfolio() {
     var element = document.getElementById('portfolio');
     element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
+}
+
+function scrollToArt() {
+    var element = document.getElementById('art');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToHobbies() {
+    var element = document.getElementById('hobbies');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
