f915e9409fe5e76faffb2fc6a24905c6c3df7d02
tyrohellion <evanmarchesani@gmail.com>
2024-03-23 17:07:48 -0400

    polish

--- script.js
+++ script.js
@@ -4,47 +4,39 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.classList.toggle('dark-theme');
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Add event listener to toggle dark theme on button click
-    document.getElementById('dark-theme').addEventListener('click', function() {
-        document.body.classList.toggle('dark-theme');
-    });
+// Add event listener to toggle dark theme on button click
+document.getElementById('dark-theme').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', function() {
+    navbar.classList.toggle('active');
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const navbar = document.querySelector('.navbar');
 
-    hamburger.addEventListener('click', function() {
-        navbar.classList.toggle('active');
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     // Function to scroll to portfolio section
     function scrollToPortfolio() {
         var element = document.getElementById('portfolio');
         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-});
-document.addEventListener('DOMContentLoaded', function() {
+
     // Function to scroll to art section
     function scrollToArt() {
         var element = document.getElementById('About');
         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-});
-document.addEventListener('DOMContentLoaded', function() {
+
     // Function to scroll to hobbies section
     function scrollToHobbies() {
         var element = document.getElementById('footer');
         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-});
-document.addEventListener('DOMContentLoaded', function() {
+
     // Add event listener to toggle dark theme on hamburger click
     document.getElementById('hamburger').addEventListener('click', function() {
         document.body.classList.toggle('dark-theme');
     });
-});
